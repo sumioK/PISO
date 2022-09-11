0c7e065e77f74673cfe1341775d43d10571bf43d
Sumio K <tap.miwo@gmail.com>
2022-09-11 10:03:39 +0900

    edit without followfunction

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+    def create
+        @following = @current_user.relationships.build(follower_id: params[:user_id])
+        @following.save
+        redirect_to request.referrer || "/"
+    end
+    
+    def destroy
+        @following = @current_user.relationships.find_by(follower_id: params[:user_id])
+        @following.destroy
+        redirect_to request.referrer || "/"
+    end
+    
+    def followings
+        user = User.find_by(id: params[:user_id])
+        @users = user.followings
+    end
+    
+    def followers
+        user = User.find_by(id: params[:user_id])
+        @users = user.followers
+    end
+
+end
