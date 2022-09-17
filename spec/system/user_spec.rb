f7d56de74b3dc37b60356357869c8279b032ec7c
Sumio K <tap.miwo@gmail.com>
2022-09-17 14:11:17 +0900

    edit docker-comopse.yml

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,53 @@
+RSpec.describe 'Users', type: :system do
+    describe 'without login' do
+        describe 'sign up' do
+            context 'フォームの入力値が正常' do
+                it 'ユーザーの新規作成が成功する' do
+                    ユーザー新規作成ページへアクセス
+                    ユーザー名を入力
+                    アドレスを入力
+                    パスワードを入力
+                    確認用パスワードを入力する
+                    登録ボタンを推す
+                    ユーザー画面に遷移する
+                end
+            end
+            context 'メールアドレスが未入力' do
+                it 'ユーザーの新規作成が失敗する'
+            end
+            context '登録済のメールアドレスを使用' do
+                it 'ユーザーの新規作成が失敗する'
+            end
+        end
+    end
+
+    describe 'マイページ' do
+        context 'ログインしていない状態' do
+            it 'マイページへのアクセスが失敗する'
+        end
+        context 'ログインしていない状態' do
+            it 'ユーザー一覧のページが表示されない'
+        end
+        context 'ログインしていない状態' do
+            it '投稿ページへのアクセスが失敗する'
+        end
+    end
+
+    describe 'ログイン後' do
+        before { login_as(user) }
+
+        describe 'ユーザー編集' do
+            context 'フォームの入力値が正常' do
+                it 'ユーザーの編集が成功する'
+            end
+            context '他ユーザーの編集ページにアクセス' do
+                it '編集ページへのアクセスが失敗する'
+            end
+        end
+        describe 'マイページ' do
+            context 'タスクを作成' do
+                it '新規作成したタスクが表示される'
+            end
+        end
+    end
+end
