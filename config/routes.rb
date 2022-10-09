4bfb0290057eea6ce058c611cd1055c7ae64fd27
Sumio K <tap.miwo@gmail.com>
2022-10-09 16:05:10 +0900

    add comment.length

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,8 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   get 'posts/new' => 'posts#new'
   get 'posts/index' => 'posts#index'
-  get 'posts/limit' => 'posts#limitindex'
-  get 'posts/:id' => 'posts#show'
   get 'posts/serch' => 'posts#serch'
+  get 'posts/:id' => 'posts#show'
 
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
